5809711a5bc4ca9618911fe446c575c883c1737b
Ivan Astudillo <ivankansasc@gmail.com>
2019-08-22 19:38:22 -0500

    update

---
+++ augustFifthhw.js
@@ -0,0 +1,109 @@
+!DOCTYPE>
+
+<body>
+    <h2>JavaScript</h2>
+    
+    <p id="demo"></p>
+
+
+    
+
+</body>
+
+    var text="";
+    var i=o;
+    while (i < 10) {
+        text += "The number is " + i;
+        i++;
+    }
+console.log(text);
+
+
+    var text ="";
+    var i = o;
+
+    do {
+        text += " The number is " +i;
+        i++;
+}
+while (i < 10);
+console.log(text)
+
+
+
+for (i = o; i < 10; i++){
+    text += "The number is" + i;
+    }
+
+    console.log(text)
+
+
+
+
+    var text="";
+
+    for(i=o;i<3;i++)
+    {
+    for (j = o; j <2;j++)
+    {
+    text += "The number is "+j;
+
+    text += "The number is "+ i;
+    }
+    }
+    console.log(text)
+
+
+
+var text="";
+var i=o;
+for(;i<5;i++)
+{
+ text += "The number is"+ i;
+}
+console.log(text)
+
+
+
+
+var text="";
+var i=o;
+for(;i<5;)
+{
+    text +="The value is" +i;
+i++;
+}
+console.log(text)
+
+
+
+var chapters ={1:"Loops",2:"Decision",3:"Arrays"};
+
+var text ="";
+var i;
+for(i in chapters){
+    text += chapters[i];
+    text; 
+}
+console.log(text)
+
+
+
+var text ="";
+
+for(i=o;i<3;i++)
+{
+        text=text+"This value is"+i;
+        text=text;
+    var j=o;
+          while(j<2)
+{
+text=text+"The value is "+j;
+text=text;
+j++;
+}
+}
+
+console.log(text)
+
+
---
+++ looPs/scriPts.js
@@ -0,0 +1,12 @@
+var text="";
+
+    for(i=0;i<3;i++)
+    {
+    for (j =5 ; j <2;j++)
+    {
+    text += "The number is "+j;
+
+    text += "The number is "+ i;
+    }
+    }
+console.log(text)
