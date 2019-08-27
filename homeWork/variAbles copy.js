3ffa0c08b81d2f1d4782e46ac62af042668b954d
Ivan Astudillo <ivankansasc@gmail.com>
2019-08-26 20:17:20 -0500

    AppWork

---
+++ homeWork/variAbles copy.js
@@ -0,0 +1,43 @@
+var firstString = "";
+var secondString = "Im not that old im only 2";
+var middleName = "nathaniel T grove";
+var parrotName = "someName";
+var dogName = "another Name ";
+var paragraph = "this is a hard to read paragraph";
+// Above are the examples of a String. 
+// Strings can represent words, paragraphs, or
+// anything that needs to be text.
+
+var num1, num2, num3;
+// Above shows how to create multiple variables at once.
+// very useful when trying to keep lines used in code 
+// at minimum. Below shows how to use those variables....
+num1 = 1000;
+num2 = 100000;
+num3 = 100000000;
+
+var name, carCapacity, region;
+name = 100;
+carCapacity = "six people";
+region = name + carCapacity;
+// Variables dont have to be created for the name
+// Create variables to hold data. 
+var smallDecimal = 10.46;
+var anotherDecimal = 1.3;
+var decimalThree = 33.02;
+var twoD = anotherDecimal * decimalThree;
+var threeDEE = twoD + anotherDecimal;
+// Above represents the data type Decimal in
+// use with Variables.
+var notTrue = true;
+var soTrue = false;
+var theNextOne = notTrue;
+var anotherBoolean = true;
+// Booleans are True - False values. They are very useful
+// inside of conditions. In order for you to be able to 
+// enhance your code and make it a little more responsive
+// you need to add conditionals. True / False conditions 
+// that start or stop your code.
+var myFisrtarray=[];
+var x=[46,45,44];
+var y={A:"A1"};
---
+++ homeWork/variablesTest copy.js
@@ -0,0 +1,51 @@
+var addingNumbers= 23 + 45;
+
+var sketch = true;
+
+var dog = false
+
+var x ="Word";
+
+var x = 700;
+
+var convertNum = 1;
+
+var boo1 = true;
+
+var scholl = false;
+
+var y= 12
+
+var you = 26
+
+
+
+
+
+
+
+
+var k= 10
+for (i = 0; i < k; i++) {
+console.log("the value of I is:"+ i)
+
+}
+
+
+
+
+for (var i = 0; i < 25; i++) {
+    var text = "";
+    text += "Tenisha";
+    console.log(text);
+
+}
+
+
+
+for (var i = 0; i < 25; i+5) {
+    var text = "";
+    text += "Tenisha";
+    console.log(text);
+
+}
